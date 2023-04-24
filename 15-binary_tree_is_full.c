1a65d206e5fb33f90eeaee3313bb774272385a5b
carlalap <carlos3_alarcon@hotmail.com>
2023-04-23 20:01:56 -0700

    betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	/* If tree is empty, it's full */
 	if (tree == NULL)
-		return (1);
+		return (0);
 
 	/* If node is a leaf, it's full */
 	if (tree->left == NULL && tree->right == NULL)
